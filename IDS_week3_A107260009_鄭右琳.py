77cbdb875be3b44e3e59c5b5371982e8725f6f6b
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-21 17:36:37 +0800

    Created using Colaboratory

---
+++ IDS_week3_A107260009_鄭右琳.py
@@ -0,0 +1,93 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "week3-A107260009-鄭右琳.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyO8QM2UiJMcceWZbXIHsOZG",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/yoyo1022/Introduciton-to-Data-Science/blob/master/IDS_week3_A107260009_%E9%84%AD%E5%8F%B3%E7%90%B3.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "YozjKLVaaEne",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 69
+        },
+        "outputId": "a61c8ed8-ba29-49b5-aa37-96d3c37bd2b6"
+      },
+      "source": [
+        "movie_time = \"181\"\n",
+        "movie_rating = \"8.5\"\n",
+        "movie_title = \"Avengers: Endgame\"\n",
+        "print(movie_time) \n",
+        "print(movie_rating) \n",
+        "print(movie_title)"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "181\n",
+            "8.5\n",
+            "Avengers: Endgame\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "yQqyNdzDcC2u",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 35
+        },
+        "outputId": "90fcc954-1964-4660-9777-6185c8381325"
+      },
+      "source": [
+        "type(movie_time) \n",
+        "type(movie_rating) \n",
+        "type(movie_title)"
+      ],
+      "execution_count": 5,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "str"
+            ]
+          },
+          "metadata": {
+            "tags": []
+          },
+          "execution_count": 5
+        }
+      ]
+    }
+  ]
+}

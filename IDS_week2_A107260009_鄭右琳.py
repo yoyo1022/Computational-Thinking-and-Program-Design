4a633acecb8187d72faa7fbd6bfdc70044d43b14
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-21 11:28:57 +0800

    Created using Colaboratory

---
+++ IDS_week2_A107260009_鄭右琳.py
@@ -0,0 +1,98 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "week2-A107260009-鄭右琳.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyO3D348nbBrrdI0hb4aJNDh",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/yoyo1022/Introduciton-to-Data-Science/blob/master/IDS_week2_A107260009_%E9%84%AD%E5%8F%B3%E7%90%B3.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "AOVKYd0UHTLB",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 35
+        },
+        "outputId": "c5d9b9c1-323c-4b2f-810a-4f38b56413fa"
+      },
+      "source": [
+        "print(\"Hello World\")"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Hello World\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "_sM8uxwFH0Kl",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 381
+        },
+        "outputId": "4c970d40-ab65-4d7a-e039-ce33e7697546"
+      },
+      "source": [
+        "import this"
+      ],
+      "execution_count": 2,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "The Zen of Python, by Tim Peters\n",
+            "\n",
+            "Beautiful is better than ugly.\n",
+            "Explicit is better than implicit.\n",
+            "Simple is better than complex.\n",
+            "Complex is better than complicated.\n",
+            "Flat is better than nested.\n",
+            "Sparse is better than dense.\n",
+            "Readability counts.\n",
+            "Special cases aren't special enough to break the rules.\n",
+            "Although practicality beats purity.\n",
+            "Errors should never pass silently.\n",
+            "Unless explicitly silenced.\n",
+            "In the face of ambiguity, refuse the temptation to guess.\n",
+            "There should be one-- and preferably only one --obvious way to do it.\n",
+            "Although that way may not be obvious at first unless you're Dutch.\n",
+            "Now is better than never.\n",
+            "Although never is often better than *right* now.\n",
+            "If the implementation is hard to explain, it's a bad idea.\n",
+            "If the implementation is easy to explain, it may be a good idea.\n",
+            "Namespaces are one honking great idea -- let's do more of those!\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}

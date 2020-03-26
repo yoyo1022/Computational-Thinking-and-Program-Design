c2b48617e19945f531413d2747ffa2a50007d9f2
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-27 00:51:05 +0800

    Delete week3.py

--- IDS_week3/week3.py
+++
@@ -1,9 +0,0 @@
-movie_time = "181"
-movie_rating = "8.5"
-movie_title = "Avengers: Endgame"
-print(movie_time) 
-print(movie_rating) 
-print(movie_title)
-type(movie_time) 
-type(movie_rating) 
-type(movie_title)

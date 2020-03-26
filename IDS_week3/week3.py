424d90e20651e414234c72ea96b162bbbe12f7c1
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-27 00:52:09 +0800

    week3_作業

---
+++ IDS_week3/week3.py
@@ -0,0 +1,11 @@
+movie_time = "181"
+movie_rating = "8.5"
+movie_title = "Avengers: Endgame"
+
+print(movie_time) 
+print(movie_rating) 
+print(movie_title)
+
+print(type(movie_time)) 
+print(type(movie_rating)) 
+print(type(movie_title))

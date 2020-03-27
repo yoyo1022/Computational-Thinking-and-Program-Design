ecdc8cf63754c2c6857319c0950e044b82f8c305
yoyo1022 <61856767+yoyo1022@users.noreply.github.com>
2020-03-27 15:04:36 +0800

    Created using Colaboratory

---
+++ IDS_week3_A107260009_鄭右琳.py
@@ -0,0 +1,69 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "week3-A107260009-鄭右琳.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyMkUSTFm300RhCKQ2kU7ZWC",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/yoyo1022/Introduciton-to-Data-Science/blob/master/IDS_week3_A107260009_%E9%84%AD%E5%8F%B3%E7%90%B3.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "YozjKLVaaEne",
+        "colab_type": "code",
+        "outputId": "7d4432c0-ac3b-40d8-8559-6b465e7356aa",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 121
+        }
+      },
+      "source": [
+        "movie_time = 181\n",
+        "movie_rating = 8.5\n",
+        "movie_title = \"Avengers: Endgame\"\n",
+        "\n",
+        "print(movie_time) \n",
+        "print(movie_rating) \n",
+        "print(movie_title)\n",
+        "\n",
+        "print(type(movie_time)) \n",
+        "print(type(movie_rating)) \n",
+        "print(type(movie_title))"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "181\n",
+            "8.5\n",
+            "Avengers: Endgame\n",
+            "<class 'int'>\n",
+            "<class 'float'>\n",
+            "<class 'str'>\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
